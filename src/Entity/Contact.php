11863c9747e17553a89f4a6c148582a8330ff836
theophile <theophile.cosse@orange.fr>
2020-01-12 19:33:44 +0100

    fix: remove the contact entity from database

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -2,95 +2,105 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
- */
 class Contact
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @var string|null
      * @Assert\NotBlank()
      */
     private $firstname;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @var string|null
      * @Assert\NotBlank()
      */
     private $lastname;
 
     /**
-     * @ORM\Column(type="string", length=3200)
+     * @var string|null
      * @Assert\NotBlank()
      */
     private $message;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @var string|null
      * @Assert\NotBlank()
      * @Assert\Email()
      */
     private $mail;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
+    /**
+     * @return string|null
+     */
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
 
-    public function setFirstname(string $firstname): self
+    /**
+     * @param string|null $firstname
+     * @return Contact
+     */
+    public function setFirstname(?string $firstname): Contact
     {
         $this->firstname = $firstname;
-
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getLastname(): ?string
     {
         return $this->lastname;
     }
 
-    public function setLastname(string $lastname): self
+    /**
+     * @param string|null $lastname
+     * @return Contact
+     */
+    public function setLastname(?string $lastname): Contact
     {
         $this->lastname = $lastname;
-
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string|null $message
+     * @return Contact
+     */
+    public function setMessage(?string $message): Contact
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
     public function getMail(): ?string
     {
         return $this->mail;
     }
 
-    public function setMail(string $mail): self
+    /**
+     * @param string|null $mail
+     * @return Contact
+     */
+    public function setMail(?string $mail): Contact
     {
         $this->mail = $mail;
-
-        return $this;
-    }
-
-    public function getMessage()
-    {
-        return $this->message;
-    }
-
-    public function setMessage($message)
-    {
-        $this->message = $message;
         return $this;
     }
 
---
+++ src/Migrations/Version20200112182712.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200112182712 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE contact');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, lastname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, message VARCHAR(3200) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, mail VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX UNIQ_4C62E63883A00E68 (firstname), UNIQUE INDEX UNIQ_4C62E6385126AC48 (mail), UNIQUE INDEX UNIQ_4C62E6383124B5B6 (lastname), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+    }
+}
