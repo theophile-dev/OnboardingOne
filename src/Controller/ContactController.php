43fdb28792256f35b2b274b8930941dfc321bd00
theophile <theophile.cosse@orange.fr>
2020-01-15 00:38:28 +0100

    fix: change submit button name

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -13,19 +13,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ContactController extends AbstractController
 {
-
     public function index(Request $request, ContactNotification $notification): Response {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            
+
             // Send an email to the department's manager
             $notification->notify($contact);
 
             // Comment this line if you want to try sending multiple form without reloading
-            return  $this->redirectToRoute("");
+            //return  $this->redirectToRoute("");
         }
         return $this->render('pages/contact.html.twig', ['form' => $form->createView()]);
     }
