08cc385be8daa44d911c24fcb5f6f3b648476e4f
theophile <theophile.cosse@orange.fr>
2020-01-19 18:25:18 +0100

    feat: contact is now stored in the database

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,16 +4,30 @@
 namespace App\Controller;
 
 
+
 use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Notification\ContactNotification;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ContactController extends AbstractController
 {
-    public function index(Request $request, ContactNotification $notification): Response {
+    /**
+     * Display the form, if the form is valid, store the contact data
+     * and send an email using ContactNotification
+     *
+     * @param Request $request
+     * @param ContactNotification $notification
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function index(Request $request, ContactNotification $notification, EntityManagerInterface $entityManager): Response {
+
+        $entityManager = $this->getDoctrine()->getManager();
+
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
@@ -25,6 +39,9 @@ class ContactController extends AbstractController
 
             // Comment this line if you want to try sending multiple form without reloading
             // return  $this->redirectToRoute('/');
+
+            $entityManager->persist($contact);
+            $entityManager->flush();
         }
         return $this->render('pages/contact.html.twig', ['form' => $form->createView()]);
     }
--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -2,129 +2,114 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-class Contact {
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
     /**
-     * @var string|null
+     * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
      */
     private $firstname;
 
     /**
-     * @var string|null
+     * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
      */
     private $lastname;
 
     /**
-     * @var string|null
+     * @ORM\Column(type="text")
      * @Assert\NotBlank()
      */
     private $message;
 
     /**
-     * @var string|null
+     * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
      * @Assert\Email()
      */
     private $email;
 
     /**
-     * @var Department|null
-     * @Assert\NotBlank()
+     * @ORM\ManyToOne(targetEntity="App\Entity\Department")
+     * @ORM\JoinColumn(nullable=false)
      */
     private $department;
 
-    /**
-     * @return string|null
-     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
 
-    /**
-     * @param string|null $firstname
-     * @return Contact
-     */
-    public function setFirstname(?string $firstname): Contact
+    public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
+
         return $this;
     }
 
-    /**
-     * @return string|null
-     */
     public function getLastname(): ?string
     {
         return $this->lastname;
     }
 
-    /**
-     * @param string|null $lastname
-     * @return Contact
-     */
-    public function setLastname(?string $lastname): Contact
+    public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
+
         return $this;
     }
 
-    /**
-     * @return string|null
-     */
     public function getMessage(): ?string
     {
         return $this->message;
     }
 
-    /**
-     * @param string|null $message
-     * @return Contact
-     */
-    public function setMessage(?string $message): Contact
+    public function setMessage(string $message): self
     {
         $this->message = $message;
+
         return $this;
     }
 
-    /**
-     * @return string|null
-     */
     public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    /**
-     * @param string|null $email
-     * @return Contact
-     */
-    public function setEmail(?string $email): Contact
+    public function setEmail(string $email): self
     {
         $this->email = $email;
+
         return $this;
     }
 
-    /**
-     * @return Department|null
-     */
     public function getDepartment(): ?Department
     {
         return $this->department;
     }
 
-    /**
-     * @param Department|null $department
-     * @return Contact
-     */
-    public function setDepartment(?Department $department): Contact
+    public function setDepartment(?Department $department): self
     {
         $this->department = $department;
+
         return $this;
     }
-
 }
